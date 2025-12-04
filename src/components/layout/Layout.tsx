883b302cbb021acbb657781b776a774b54b81f88
italic-jinxin <106428113+italic-jinxin@users.noreply.github.com>
2025-12-04 21:44:10 +0800

    feat: sync remote settings

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,8 +3,25 @@ import { Outlet } from "react-router";
 import ChatVerifier from "@/components/chat/verifier/ChatVerifier";
 import LeftSidebar from "@/components/sidebar";
 import ImportGuideBanner from "@/components/common/ImportGuideBanner";
+import { useUserSettings } from "@/api/users/queries";
+import { useChatStore } from "@/stores/useChatStore";
+import { useTheme } from "../common/ThemeProvider";
+import { useEffect } from "react";
 
 const Layout: React.FC = () => {
+  const { data: remoteSettings } = useUserSettings();
+  const { setTheme } = useTheme();
+  const { setWebSearchEnabled } = useChatStore();
+
+  useEffect(() => {
+    if (remoteSettings?.settings) {
+      if (remoteSettings.settings.appearance) {
+        setTheme(remoteSettings.settings.appearance);
+      }
+      setWebSearchEnabled(remoteSettings.settings.web_search);
+    }
+  }, [remoteSettings, setTheme, setWebSearchEnabled]);
+
   return (
     <div className="flex h-screen w-full flex-row">
       <LeftSidebar />
