65e5974e4c0b9130273fe041659185111c380890
italic-jinxin <106428113+italic-jinxin@users.noreply.github.com>
2025-12-04 10:47:32 +0800

    chore: remove docs

--- src/pages/PrivacyPage.tsx
+++
@@ -1,9 +0,0 @@
-const PrivacyPage = () => {
-  return (
-    <main className="h-screen max-h-dvh w-full overflow-scroll">
-      <iframe src="/docs/privacy.pdf" width="100%" height="100%" />
-    </main>
-  );
-};
-
-export default PrivacyPage;
