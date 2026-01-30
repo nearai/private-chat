1d243d21c2e6379f4e70e1b27d832423925eb873
Robert Yan <46699230+think-in-universe@users.noreply.github.com>
2026-01-30 18:35:47 +0800

    chore: hide add model button (#302)

--- src/components/chat/ModelSelector.tsx
+++ src/components/chat/ModelSelector.tsx
@@ -165,7 +165,8 @@ export default function ModelSelector() {
     return models.filter((modelId) => !otherSelectedModels.includes(modelId.modelId));
   };
 
-  const disabledAdd = selectedModels.length >= models.length || selectedModels.length >= 3;
+  // TODO: Temporary disabled add model button before we fix the `previous_response_id` issue for multiple models in cloud-api.
+  const disabledAdd = true; // selectedModels.length >= models.length || selectedModels.length >= 3;
 
   return (
     <div className="flex w-full flex-col items-start">
