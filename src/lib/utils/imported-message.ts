18e4a250584d3c0bce7d21825ba7a4df0976b832
jinxin <106428113+italic-jinxin@users.noreply.github.com>
2026-02-02 18:27:45 +0800

    chore: minor

--- src/lib/utils/imported-message.ts
+++ src/lib/utils/imported-message.ts
@@ -1,9 +1,8 @@
 import type { Conversation, ConversationItem } from "@/types";
 import dayjs from "dayjs";
-import { MOCK_MESSAGE_RESPONSE_ID_PREFIX } from "../constants";
+import { IMPORT_TIMESTAMP_TOLERANCE_SECONDS, MOCK_MESSAGE_RESPONSE_ID_PREFIX } from "../constants";
 
 export function isImportedMessage(conversation: Conversation, message: ConversationItem): boolean {
-    if (!conversation.metadata) return false;
     if (!message) return false;
     const importedAtStr = conversation.metadata?.imported_at;
     if (!importedAtStr) return false;
@@ -12,5 +11,5 @@ export function isImportedMessage(conversation: Conversation, message: Conversat
     }
     const importedAt = dayjs(Number(importedAtStr));
     const msgCreatedAt = dayjs(message.created_at * 1000);
-    return Math.abs(msgCreatedAt.diff(importedAt, 'second')) < 59;
+    return Math.abs(msgCreatedAt.diff(importedAt, 'second')) < IMPORT_TIMESTAMP_TOLERANCE_SECONDS;
 }
